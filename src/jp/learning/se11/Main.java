961c10a91c447e0441ea1649a8be5174aca6380f
toru <t02f447f@gmail.com>
2020-11-18 05:09:08 +0900

    JavaSE11 BlackBook practice p344

--- src/jp/learning/se11/Main.java
+++ src/jp/learning/se11/Main.java
@@ -7,5 +7,15 @@ public class Main {
     List<String> list = List.of("A", "B", "C");
     list.forEach(str -> System.out.println(str));
     list.forEach(System.out::println);
+
+    //344ページ
+    try{
+      int[] array = {};
+      array[0] = 10;
+      System.out.println("finish");
+    } catch(ArrayIndexOutOfBoundsException e) {
+      System.out.println("error");
+
+    }
   }
 }
