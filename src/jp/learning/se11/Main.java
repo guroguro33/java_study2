c4cae1903a3d6d0c35fb702700d8229956a8619f
toru <t02f447f@gmail.com>
2020-11-19 05:14:50 +0900

    JavaSE11 BlackBook practice p336

--- src/jp/learning/se11/Main.java
+++ src/jp/learning/se11/Main.java
@@ -1,12 +1,27 @@
 package jp.learning.se11;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
   public static void main(String[] args){
     List<String> list = List.of("A", "B", "C");
     list.forEach(str -> System.out.println(str));
     list.forEach(System.out::println);
+
+    //336ページ
+    List<String> list2 = List.of("A", "B", "C");
+    Consumer<String> logic2 = new Consumer<String>(){
+      @Override
+      public void accept(String s) {
+        System.out.println(s);
+      }
+    };
+    list2.forEach(logic2);
+
+    //ラムダ式で
+    Consumer<String> logic3 = (s) -> System.out.println(s);
+    list2.forEach(logic3);
 
     //344ページ
     try{
