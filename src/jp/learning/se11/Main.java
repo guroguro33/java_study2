6815a884d03ec8274bb24ac0e0ddb57ab0c4a0a2
toru <t02f447f@gmail.com>
2020-11-21 06:29:35 +0900

    JavaSE11 BlackBook practice p397

--- src/jp/learning/se11/Main.java
+++ src/jp/learning/se11/Main.java
@@ -40,5 +40,15 @@ public class Main {
 
     Sample a = new Sample();
     a.sample();
+
+    // 397ページ
+    int data = 1;
+    switch(data){
+      default: System.out.print("C");
+      case 0: System.out.print("A");
+        break;
+      case 10: System.out.print("B");
+        break;
+    }
   }
 }
