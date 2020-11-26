70af96a1e9c41d7261f10a1657f516e1f833a593
toru <t02f447f@gmail.com>
2020-11-27 05:13:15 +0900

    JavaSE11 BlackBook practice p423

---
+++ src/jp/learning/se11_3/A.java
@@ -0,0 +1,7 @@
+package jp.learning.se11_3;
+
+public class A {
+  protected boolean isValue(int val){
+    return true;
+  }
+}
---
+++ src/jp/learning/se11_3/B.java
@@ -0,0 +1,14 @@
+package jp.learning.se11_3;
+
+public class B extends A{
+  public int test(int num){
+    if(isValue(num)){
+      return num;
+    }
+    return 0;
+  }
+  public static void main(String[] args){
+    B b = new B();
+    System.out.println(b.test(10));
+  }
+}
