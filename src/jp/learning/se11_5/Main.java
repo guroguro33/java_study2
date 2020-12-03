7da265d0ca66c22965ddc570826c2c52c2476c0a
toru <t02f447f@gmail.com>
2020-12-04 04:51:55 +0900

    JavaSE11 BlackBook practice p502

--- src/jp/learning/se11_5/Main.java
+++ src/jp/learning/se11_5/Main.java
@@ -1,5 +1,8 @@
 package jp.learning.se11_5;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
   public static void main(String[] args){
     SubSample sub = new SubSample();
@@ -7,5 +10,19 @@ public class Main {
     sub = (SubSample)sample;
     sub.test();
     sample.test();
+
+    // 502ページ
+    try {
+      test(0);
+    } catch(IOException e){
+      e.getMessage();
+    }
+  }
+  public static void test(int num) throws IOException, IndexOutOfBoundsException{
+    if(num < 10){
+      throw new FileNotFoundException();
+    } else {
+      throw new IndexOutOfBoundsException();
+    }
   }
 }
