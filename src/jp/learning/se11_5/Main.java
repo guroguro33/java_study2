af377fc689ad4169073f29eb25b56d775aba3311
toru <t02f447f@gmail.com>
2020-12-05 07:30:51 +0900

    JavaSE11 BlackBook practice p510

--- src/jp/learning/se11_5/Main.java
+++ src/jp/learning/se11_5/Main.java
@@ -4,6 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
+  //510ページ
+  private static int num;
+  private int num3;
+  static{
+    num = 10;
+  }
+  static{
+    num = 20;
+  }
+  static void test2(int num){
+    num = num * num;
+  }
   public static void main(String[] args){
     SubSample sub = new SubSample();
     Sample sample = new SubSample();
@@ -17,6 +29,11 @@ public class Main {
     } catch(IOException e){
       e.getMessage();
     }
+
+    //510ページ
+    test2(num);
+    System.out.println(num);
+
   }
   public static void test(int num) throws IOException, IndexOutOfBoundsException{
     if(num < 10){
