e7b2b397df2bf4ecf6e3d9a2828e1ed8dca6dab7
toru <t02f447f@gmail.com>
2020-12-09 04:55:09 +0900

    JavaSE11 BlackBook practice p545

---
+++ src/jp/learning/se11_7/A.java
@@ -0,0 +1,10 @@
+package jp.learning.se11_7;
+
+import java.util.List;
+import java.util.Set;
+
+public class A {
+  public List<Number> test(Set<CharSequence> s){
+    return null;
+  }
+}
---
+++ src/jp/learning/se11_7/B.java
@@ -0,0 +1,9 @@
+package jp.learning.se11_7;
+
+import java.util.TreeSet;
+
+public class B extends A{
+  public boolean test(TreeSet<String> s){
+    return false;
+  };
+}
